e7b8f1e5c0b43ba13f82878afcb5b0d2a5080780
Марс <profimars@mail.ru>
2023-03-01 23:32:32 +0300

    Добавил задачу 4

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿//Задача 4
+//Напишите программу, которая на вход принимает три числа, и выдает максимальное из этих чисел
+//2; 3; 7
+//44; 5; 78
+//22; 3; 9
+Console.WriteLine("Введите первое число");
+int q1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int q2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int q3 = Convert.ToInt32(Console.ReadLine());
+int[] nn = { q1, q2, q3 };
+int max = nn[0];
+int i = 0;
+int size = 3;
+while (i < size)
+{
+    if (nn[i] > max)
+        max = nn[i];
+    i++;
+}
+Console.WriteLine("Максимальное число = " + max);
